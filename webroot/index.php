a30880010c3b1c0292189af69232cbb4abd91ec8
Adam C <adam@swimstandards.com>
2025-06-05 09:17:18 -0400

    use  correct date forat to check current meet

--- webroot/index.php
+++ webroot/index.php
@@ -4,18 +4,16 @@ require_once __DIR__ . '/../lib/bootstrap.php';
 
 $currentMeet = null;
 
-
 if (!empty($_ENV['MONGODB_URI'])) {
   require_once __DIR__ . '/../lib/mongodb.php';
   $mongo = new MongoDBLibrary();
 
-  $now = new MongoDB\BSON\UTCDateTime((new DateTime())->getTimestamp() * 1000);
+  $todayStr = (new DateTime())->format('Y-m-d');
 
-  // Only fetch meet where today is between start and end dates
   $doc = $mongo->collection->findOne([
-    'type' => 'psych_sheets', // or 'result' or any type you expect to exist for a current meet
-    'meet_start_date' => ['$lte' => $now],
-    'meet_end_date' => ['$gte' => $now]
+    'type' => 'psych_sheets',
+    'meet_start_date' => ['$lte' => $todayStr],
+    'meet_end_date' => ['$gte' => $todayStr]
   ], [
     'sort' => ['meet_start_date' => -1]
   ]);
@@ -33,7 +31,6 @@ if (!empty($_ENV['MONGODB_URI'])) {
     ];
   }
 }
-// pr_pre($currentMeet);
 
 $templates->addData([
   'meta_title' => 'SwimSnap – Community-Powered Swim Meet Info',
